98d9cfd0f73a1c392033fa63cf57486bae9fd94d
Ivan Grushevich <githubmail911@gmail.com>
2017-09-20 16:01:32 +0200

    implement class GuessingGame

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
 
     guess() {
-
+      this.assump = Math.ceil( (this.max + this.min) / 2 );
+      return this.assump;
     }
 
     lower() {
-
+      this.max = this.assump;
     }
 
     greater() {
-
+      this.min = this.assump;
     }
 }
 
